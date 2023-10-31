26dd3eec46e473d8a2b687adb72ac6ef7528c13a
a-terumoto-gs <a.terumoto@genbasupport.com>
2023-10-31 13:10:55 +0900

    nameとpostal_codeにバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-  has_many :tasks, dependent:  :destroy    
+  has_many :tasks, dependent:  :destroy
+  validates :name, presence: true
+  validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/}
 end
--- db/migrate/20231027072309_add_name_and_postal_code_and_address_and_profile_to_users.rb
+++ db/migrate/20231027072309_add_name_and_postal_code_and_address_and_profile_to_users.rb
@@ -1,9 +1,8 @@
 class AddNameAndPostalCodeAndAddressAndProfileToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :name, :string
-    add_column :users, :postal_code, :string
+    add_column :users, :name, :string, null: false, default: ""
+    add_column :users, :postal_code, :strin
     add_column :users, :address, :string
-    add_column :users, :profile, :text
-
+    add_column :users, :profile, :te
   end
 end

104dd624f5d2ed91bd42a3988aa371f1a3bbf87a
a-terumoto-gs <a.terumoto@genbasupport.com>
2023-11-01 14:47:22 +0900

    各ページへのリンクを追加

--- db/migrate/20231027072309_add_name_and_postal_code_and_address_and_profile_to_users.rb
+++ db/migrate/20231027072309_add_name_and_postal_code_and_address_and_profile_to_users.rb
@@ -1,7 +1,7 @@
 class AddNameAndPostalCodeAndAddressAndProfileToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :name, :string, null: false, default: ""
-    add_column :users, :postal_code, :strin
+    add_column :users, :postal_code, :strins
     add_column :users, :address, :string
     add_column :users, :profile, :te
   end

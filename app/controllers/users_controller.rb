1db2b82661a0cd10189402d4ea0c545c973b486c
a-terumoto-gs <a.terumoto@genbasupport.com>
2023-11-06 15:20:14 +0900

    画像を設定できるよう変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,26 @@
 
 class UsersController < ApplicationController
   def index
-    @users = User.order(:id).page(params[:page])
+    @users = User.order(:id).page(params[:page]).includes(avatar_attachment: :blob)
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_url, notice: 'Add User'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.fetch(:user, {}).permit(:name, :avatar)
   end
 end

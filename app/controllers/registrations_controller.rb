bad3845293f415f4541f766c6d2e037453a61cbe
a-terumoto-gs <a.terumoto@genbasupport.com>
2023-11-08 13:50:47 +0900

    ユーザーの属性追加方法の見直し

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,22 +1,6 @@
 # frozen_string_literal: true
 
 class RegistrationsController < Devise::RegistrationsController
-  def create
-    super do |resource|
-      resource.name = params[:user][:name]
-      resource.postal_code = params[:user][:postal_code]
-      resource.address = params[:user][:address]
-      resource.profile = params[:user][:profile]
-      resource.save
-    end
-  end
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :postal_code, :address, :profile)
-  end
-
-  protected
-
   def after_update_path_for(resource)
     user_path(resource)
   end

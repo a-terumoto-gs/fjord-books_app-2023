501fabfd42a548b4dbb9e23c1b9793628730c144
a-terumoto-gs <a.terumoto@genbasupport.com>
2023-11-15 15:24:05 +0900

    メンション付けできるように

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -22,6 +22,9 @@ class ReportsController < ApplicationController
     @report = current_user.reports.new(report_params)
 
     if @report.save
+      contents = @report.content.scan(%r{http://localhost:3000/reports/(\d{1,})})
+      set_mention(contents)
+      
       redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
     else
       render :new, status: :unprocessable_entity
@@ -30,6 +33,11 @@ class ReportsController < ApplicationController
 
   def update
     if @report.update(report_params)
+      mentions = Mention.where(mentioning_report_id: @report.id)
+      mentions.each(&:destroy)
+
+      contents = @report.content.scan(%r{http://localhost:3000/reports/(\d{1,})})
+      set_mention(contents)
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
       render :edit, status: :unprocessable_entity
@@ -50,5 +58,16 @@ class ReportsController < ApplicationController
 
   def report_params
     params.require(:report).permit(:title, :content)
+  end
+
+  def set_mention(contents)
+    unless contents.empty?
+      contents.each do |content|
+        mention = Mention.new
+        mention.mentioning_report_id = @report.id
+        mention.mentioned_report_id = content[0]
+        mention.save
+      end
+    end
   end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -4,6 +4,12 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
+  has_many :mentioned, class_name: 'Mention', foreign_key: :mentioned_report_id, inverse_of: :mentioned_report, dependent: :destroy
+  has_many :mentioning_reports, through: :mentioned, source: :mentioning_report
+
+  has_many :mentioning, class_name: 'Mention', foreign_key: :mentioning_report_id, inverse_of: :mentioning_report, dependent: :destroy
+  has_many :mentioned_reports, through: :mentioning, source: :mentioned_report
+
   validates :title, presence: true
   validates :content, presence: true
 

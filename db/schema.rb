a39f1c85f6a081d82f9ef087b7751edc44208faf
a-terumoto-gs <a.terumoto@genbasupport.com>
2023-11-15 17:58:33 +0900

    データベーススキーマの状態の設定しなおし

--- db/schema.rb
+++ db/schema.rb
@@ -48,16 +48,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_06_013430) do
     t.string "picture"
   end
 
-  create_table "tasks", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "title"
-    t.text "body"
-    t.integer "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_tasks_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -76,5 +66,4 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_06_013430) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "tasks", "users"
 end

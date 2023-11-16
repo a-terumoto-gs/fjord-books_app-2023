f27c34336240677d7502c7be2189c8d21939ed05
a-terumoto-gs <a.terumoto@genbasupport.com>
2023-11-16 15:00:29 +0900

    マイグレーションファイルの整理

--- db/migrate/20231110012849_add_report_id_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddReportIdToComments < ActiveRecord::Migration[7.0]
-  def change
-    add_column :comments, :report_id, :integer
-  end
-end
--- db/migrate/20231110013929_add_book_id_to_comments.rb
+++
@@ -1,5 +0,0 @@
-class AddBookIdToComments < ActiveRecord::Migration[7.0]
-  def change
-    add_column :comments, :book_id, :integer
-  end
-end

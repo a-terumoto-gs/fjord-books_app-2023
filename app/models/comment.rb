7e6d019274ca6ffa993bc8b6f5d87b0eff263e89
a-terumoto-gs <a.terumoto@genbasupport.com>
2023-11-16 16:40:55 +0900

    ユーザー削除時に日報・コメントも削除されるように

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,12 +5,4 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
 
   attr_accessor :name
-
-  before_save :set_name
-
-  private
-
-  def set_name
-    self.name = user&.name || '退会済みユーザー'
-  end
 end

7d7e306db04f77c1280ff0bbbae3d26230edab7b
a-terumoto-gs <a.terumoto@genbasupport.com>
2023-11-20 13:21:07 +0900

    users_contoller.rbの不要な記述削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  private
-
-  def user_params
-    params.fetch(:user, {}).permit(:name, :avatar)
-  end
 end

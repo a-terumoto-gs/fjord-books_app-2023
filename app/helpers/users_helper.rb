5f25cafa880abdc568e333d08d8d0d60f897e241
a-terumoto-gs <a.terumoto@genbasupport.com>
2023-11-21 14:44:05 +0900

    メソッドを置くファイル変更

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,13 +4,4 @@ module UsersHelper
   def current_user_name(user)
     [user.name, user.email].find(&:present?)
   end
-
-  def create_mentions
-    contents = @report.content.scan(%r{http://localhost:3000/reports/(\d{1,})})
-    return if contents.empty?
-
-    contents.each do |content|
-      Mention.create(mentioned_report_id: @report.id, mentioning_report_id: content[0])
-    end
-  end
 end
--- app/models/mention.rb
+++ app/models/mention.rb
@@ -5,4 +5,13 @@ class Mention < ApplicationRecord
   belongs_to :mentioning_report, class_name: 'Report'
 
   validates :mentioned_report_id, uniqueness: { scope: :mentioning_report_id, message: 'custom error message' }
+
+  def create_mentions
+    contents = @report.content.scan(%r{http://localhost:3000/reports/(\d{1,})})
+    return if contents.empty?
+
+    contents.each do |content|
+      Mention.create(mentioned_report_id: @report.id, mentioning_report_id: content[0])
+    end
+  end
 end

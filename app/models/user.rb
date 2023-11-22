dd501f5316309b7356a61197e70ea71555398883
a-terumoto-gs <a.terumoto@genbasupport.com>
2023-11-22 18:03:33 +0900

    userモデルテストのパスワードの扱い修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,6 @@ class User < ApplicationRecord
   has_one_attached :avatar do |attachable|
     attachable.variant :thumb, resize_to_limit: [150, 150]
   end
-  
-  has_secure_password
 
   def name_or_email
     name.presence || email

7155c37bd75b1998151490ab41e64e9e8d6051c2
a-terumoto-gs <a.terumoto@genbasupport.com>
2023-12-05 09:30:06 +0900

    エラーメッセージの修正

--- app/models/mention.rb
+++ app/models/mention.rb
@@ -4,5 +4,5 @@ class Mention < ApplicationRecord
   belongs_to :mentioned_report, class_name: 'Report'
   belongs_to :mentioning_report, class_name: 'Report'
 
-  validates :mentioned_report_id, uniqueness: { scope: :mentioning_report_id, message: '言及の重複エラー }
+  validates :mentioned_report_id, uniqueness: { scope: :mentioning_report_id, message: 'その言及は既に存在します' }
 end

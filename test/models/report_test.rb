44a1974f0f58e140297a7ec9c1eae12691075a40
a-terumoto-gs <a.terumoto@genbasupport.com>
2023-12-07 10:30:55 +0900

    残りのreportモデルテスト修正

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -7,28 +7,28 @@ class ReportTest < ActiveSupport::TestCase
 
   def setup
     @user = users(:alice)
+    @report = reports(:first_report)
   end
 
   test 'editable?' do
-    @report = reports(:first_report)
     assert @report.editable?(@user)
+    assert_not @report.editable?(users(:bob))
   end
 
   test 'created_on' do
-    @report = reports(:first_report)
     assert_equal Time.now.to_date, @report.created_on
   end
 
   test 'save_mentions' do
-    @first_report = reports(:first_report)
-    @second_report = reports(:second_report)
-    @report = @user.reports.build(title: 'mentioning', content: "http://localhost:3000/reports/#{@first_report.id}")
+    first_report = @report
+    second_report = reports(:second_report)
+    report = @user.reports.build(title: 'mentioning', content: "http://localhost:3000/reports/#{first_report.id}")
 
-    @report.save!
-    assert_equal @report.mentioning_reports.to_a, [@first_report]
+    report.save!
+    assert_equal report.mentioning_reports.to_a, [first_report]
 
-    @report.update!(content: "http://localhost:3000/reports/#{@second_report.id}")
-    @report.mentioning_reports.reload
-    assert_equal @report.mentioning_reports.to_a, [@second_report]
+    report.update!(content: "http://localhost:3000/reports/#{second_report.id}")
+    report.mentioning_reports.reload
+    assert_equal report.mentioning_reports.to_a, [second_report]
   end
 end
